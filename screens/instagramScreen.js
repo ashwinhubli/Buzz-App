0d8a64ec4e9a05ad75c6f84844b015290d20ff22
ashwinhubli <66745007+ashwinhubli@users.noreply.github.com>
2020-11-09 18:34:46 +0530

    Add files via upload

---
+++ screens/instagramScreen.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+import {Text,View} from 'react-native'
+
+export default class InstagramScreen extends React.Component{
+    render(){
+      return(
+        <View style={{flex:1,justifyContent:'center',alignItems:'center',backgroundColor:'pink'}}>
+            <Text>Instagram</Text>
+        </View>  
+      )
+    }
+}
